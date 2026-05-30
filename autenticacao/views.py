0ca4813978086adee7f8c5f98a0e6c2ac228e1f5
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-05-30 17:37:58 -0300

    implementando no cadastro o POST e o GET

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -4,6 +4,16 @@ from django.http import HttpResponse
 
 # Create your views here.
 def cadastro(request):
+    if request.method == 'GET':
+        return render(request, 'cadastro.html')
+    elif request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        return HttpResponse(f'{usuario} - {email} - {senha} - {confirmar_senha}')
+
+
     return render(request, 'cadastro.html')
 
 def logar(request):
