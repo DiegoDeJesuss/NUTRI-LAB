ad6f99080b7ff01c4506e6ab77b1ed622d005fb4
DiegoDeJesuss <diegodejesuspaula@hotmail.com>
2026-05-30 18:21:58 -0300

    já esta reginstrando usuario no admin

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,27 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
 from .ultis import password_is_valid
-from django.shortcuts import redirect
 
 
 # Create your views here.
 def cadastro(request):
     if request.method == 'GET':
         return render(request, 'cadastro.html')
+
     elif request.method == 'POST':
         usuario = request.POST.get('usuario')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirmar_senha = request.POST.get('confirmar_senha')
 
-        
         if not password_is_valid(request, senha, confirmar_senha):
             return redirect('cadastro')
-        
+
+        try:
+            user = User.objects.create_user(
+                username=usuario,
+                email=email,
+                password=senha,
+                is_active=False
+            )
+            user.save()
+
+            return redirect('/auth/logar')
+
+        except Exception as e:
+            print(e)
+            return redirect('/auth/cadastro')
+
         return HttpResponse('Testando')
 
-
-   
 
 def logar(request):
     return render(request, 'logar.html')
